7e2e94e30470bb868874db15a2e4bd352efa9cca
cory alport <cralport@gmail.com>
2021-03-09 21:04:01 -0700

    changing to for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ var mapContainerEl = document.getElementById('map-container');
 
 var map, infoWindow;
 
-function initMap () {
+function initMap() {
 	let options = {
-		center: {lat: 43.654, lng: -79.383 },
+		center: { lat: 43.654, lng: -79.383 },
 		zoom: 8
 	};
-	
+
 
 	map = new google.maps.Map(document.getElementById("map"), options);
 
@@ -17,7 +17,7 @@ function initMap () {
 
 	// set up for user location and if users geolocation is turned off or not able. 
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(p) {
+		navigator.geolocation.getCurrentPosition(function (p) {
 			var position = {
 				lat: p.coords.latitude,
 				lng: p.coords.longitude
@@ -25,15 +25,15 @@ function initMap () {
 			infoWindow.setPosition(position);
 			infoWindow.setContent("Your Location!");
 			infoWindow.open(map);
-		}, function() {
+		}, function () {
 			handleLocationError("Geolocation service faild", map.center());
 		})
 	} else {
-		handleLocationError("No geolocation available",map.center());
+		handleLocationError("No geolocation available", map.center());
 	}
 }
 
-function handleLocationError (content, position) {
+function handleLocationError(content, position) {
 	infoWindow.setPosition(position);
 	infoWindow.setContent(content);
 	infoWindow.open(map);
@@ -60,76 +60,84 @@ function formSubmitHandler(event) {
 	console.log(event);
 };
 
+const options = ["restaurants", "gas", "parks", "movies", "groceries"];
 
-var checked = JSON.parse(localStorage.getItem('restaurants'));
-if (checked == "true") {
-    document.getElementById("restaurants").checked = true;
-}
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('restaurants'));
-    document.getElementById("restaurants").checked = checked;
+for (var i = 0; i < options.length; i++) {
+	var checked = JSON.parse(localStorage.getItem(options)); 
+
 }
 
-function save(){
-    var checkbox = document.getElementById('restaurants');
-    localStorage.setItem('restaurants', checkbox.checked);
-}
 
-var checked = JSON.parse(localStorage.getItem('gas'));
-if (checked == "true") {
-    document.getElementById("gas").checked = true;
-}
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('gas'));
-    document.getElementById("gas").checked = checked;
-}
 
-function save(){
-    var checkbox = document.getElementById('gas');
-    localStorage.setItem('gas', checkbox.checked);
-}
+// var checked = JSON.parse(localStorage.getItem("restaurants"));
+// if (checked == "true") {
+//     document.getElementById("restaurants").checked = true;
+// }
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem("restaurants"));
+//     document.getElementById("restaurants").checked = checked;
+// }
 
-var checked = JSON.parse(localStorage.getItem('parks'));
-if (checked == "true") {
-    document.getElementById("parks").checked = true;
-}
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('parks'));
-    document.getElementById("parks").checked = checked;
-}
+// function save(){
+//     var checkbox = document.getElementById("restaurants");
+//     localStorage.setItem('restaurants', checkbox.checked);
+// }
 
-function save(){
-    var checkbox = document.getElementById('parks');
-    localStorage.setItem('parks', checkbox.checked);
-}
+// var checked = JSON.parse(localStorage.getItem('gas'));
+// if (checked == "true") {
+//     document.getElementById("gas").checked = true;
+// }
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem('gas'));
+//     document.getElementById("gas").checked = checked;
+// }
 
-var checked = JSON.parse(localStorage.getItem('movies'));
-if (checked == "true") {
-    document.getElementById("movies").checked = true;
-}
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('movies'));
-    document.getElementById("movies").checked = checked;
-}
+// function save(){
+//     var checkbox = document.getElementById('gas');
+//     localStorage.setItem('gas', checkbox.checked);
+// }
 
-function save(){
-    var checkbox = document.getElementById('movies');
-    localStorage.setItem('movies', checkbox.checked);
-}
+// var checked = JSON.parse(localStorage.getItem('parks'));
+// if (checked == "true") {
+//     document.getElementById("parks").checked = true;
+// }
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem('parks'));
+//     document.getElementById("parks").checked = checked;
+// }
 
-var checked = JSON.parse(localStorage.getItem('groceries'));
-if (checked == "true") {
-    document.getElementById("groceries").checked = true;
-}
-function load(){    
-    var checked = JSON.parse(localStorage.getItem('groceries'));
-    document.getElementById("groceries").checked = checked;
-}
+// function save(){
+//     var checkbox = document.getElementById('parks');
+//     localStorage.setItem('parks', checkbox.checked);
+// }
 
-function save(){
-    var checkbox = document.getElementById('groceries');
-    localStorage.setItem('groceries', checkbox.checked);
-}
+// var checked = JSON.parse(localStorage.getItem('movies'));
+// if (checked == "true") {
+//     document.getElementById("movies").checked = true;
+// }
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem('movies'));
+//     document.getElementById("movies").checked = checked;
+// }
+
+// function save(){
+//     var checkbox = document.getElementById('movies');
+//     localStorage.setItem('movies', checkbox.checked);
+// }
+
+// var checked = JSON.parse(localStorage.getItem('groceries'));
+// if (checked == "true") {
+//     document.getElementById("groceries").checked = true;
+// }
+// function load(){    
+//     var checked = JSON.parse(localStorage.getItem('groceries'));
+//     document.getElementById("groceries").checked = checked;
+// }
+
+// function save(){
+//     var checkbox = document.getElementById('groceries');
+//     localStorage.setItem('groceries', checkbox.checked);
+// }
 
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
